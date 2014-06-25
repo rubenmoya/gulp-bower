9bfb59d67eefcfd3b45340f0b445d450cb42f9a8
rubenmoya <rmoyarodriguez@gmail.com>
2014-06-25 17:34:53 +0200

    Added support for .html files
    Added support for .html files, and watch task for live reload. In the
    future this will be with the ‘views’ folder of Node.js Application.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
+/*
+Author: Rubén Moya Rodríguez (@rubenmoya_)
+You need to define the dependencies in bower.json
+See the README.md for more information
+*/
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var connect = require('gulp-connect');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
+var notify = require('gulp-notify');
+var newer = require('gulp-newer');
 
 gulp.task('connect', function(){
 	connect.server({
@@ -11,15 +19,25 @@ gulp.task('connect', function(){
 	});
 });
 
+gulp.task('html', function() {
+	gulp.src('./src/*.html')
+		.pipe(newer('./public/'))
+		.pipe(gulp.dest('./public/'))
+		.pipe(notify("File <%= file.relative %> added correctly."))
+		.pipe(connect.reload());
+});
+
 gulp.task('stylus', function(){
 	gulp.src('./src/stylus/main.styl')
 		.pipe(stylus({use: [nib()]}))
 		.pipe(gulp.dest('./public/css/'))
+		.pipe(notify("<%= file.relative %> correctly compiled with Stylus."))
 		.pipe(connect.reload());
 });
 
 gulp.task('watch', ['connect'], function(){
 	gulp.watch('./src/stylus/*.styl', ['stylus'])
+	gulp.watch('./src/*.html', ['html'])
 });
 
-gulp.task('default', ['connect', 'stylus', 'watch']);
+gulp.task('default', ['connect', 'stylus', 'html', 'watch']);
