ead9cf0757c287d1f0b91b2166a34ce12abdc81b
rubenmoya <rmoyarodriguez@gmail.com>
2014-06-26 12:29:24 +0200

    Added vendor styles and scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,19 @@ var stylus = require('gulp-stylus');
 var nib = require('nib');
 var notify = require('gulp-notify');
 var newer = require('gulp-newer');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var flatten = require('gulp-flatten');
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var bower = require('gulp-bower');
+var bowerFiles = require('gulp-bower-files');
+var bowerSrc = require('gulp-bower-src');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('connect', function(){
 	connect.server({
+		root: ['public'],
 		port: 3000,
 		livereload: true
 	});
@@ -30,14 +40,53 @@ gulp.task('html', function() {
 gulp.task('stylus', function(){
 	gulp.src('./src/stylus/main.styl')
 		.pipe(stylus({use: [nib()]}))
+		.pipe(minifycss())
+		.pipe(rename({
+			suffix: ".min"
+		}))
 		.pipe(gulp.dest('./public/css/'))
 		.pipe(notify("<%= file.relative %> correctly compiled with Stylus."))
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', ['connect'], function(){
-	gulp.watch('./src/stylus/*.styl', ['stylus'])
-	gulp.watch('./src/*.html', ['html'])
+gulp.task('scripts', function(){
+	gulp.src('./src/scripts/main.js', './src/scripts/**/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/scripts/'))
+		.pipe(notify("Scripts ready."))
+		.pipe(connect.reload());
 });
 
-gulp.task('default', ['connect', 'stylus', 'html', 'watch']);
+gulp.task('vendor-scripts', function() {
+	bowerFiles()
+		.pipe(flatten())
+		.pipe(filter('*.js'))
+		.pipe(concat('vendor.js'))
+		.pipe(uglify())
+		.pipe(rename({
+	        suffix: ".min"
+	    }))
+		.pipe(gulp.dest('./public/scripts/'))
+		.pipe(connect.reload());
+});
+
+gulp.task('vendor-styles', function() {
+	bowerFiles()
+		.pipe(flatten())
+		.pipe(filter('*.css'))
+		.pipe(concat('vendor.css'))
+		.pipe(minifycss())
+		.pipe(rename({
+	        suffix: ".min"
+	    }))
+		.pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task('watch', ['connect'], function(){
+	gulp.watch('./src/stylus/*.styl', ['stylus']);
+	gulp.watch('./src/*.html', ['html']);
+    gulp.watch('./src/scripts/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['connect', 'html', 'stylus', 'scripts', 'vendor-scripts', 'vendor-styles', 'watch']);
